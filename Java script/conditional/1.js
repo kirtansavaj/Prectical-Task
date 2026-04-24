6667ae74202caef1c20ddbaf512dbd3029567dbf
kirtansavaj <102339342+kirtansavaj@users.noreply.github.com>
2026-04-24 09:02:22 +0530

    Add files via upload

---
+++ Java script/conditional/1.js
@@ -0,0 +1,14 @@
+// Write a JavaScript program that displays the largest integer among two integers.
+
+let n1 = parseInt(prompt("Enter the first number:"));
+let n2 = parseInt(prompt("Enter the secound number:"));
+
+
+if(n1 > n2)
+{
+    window.alert("the largest integer Int is" + n1);
+}
+else
+{
+    window.alert("the largest integer Int is" + n2);
+}
