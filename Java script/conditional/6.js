a8439e971e71b79e2663cd65c458060ba49eee7c
kirtansavaj <102339342+kirtansavaj@users.noreply.github.com>
2026-04-25 09:10:05 +0530

    Add files via upload

---
+++ Java script/conditional/6.js
@@ -0,0 +1,13 @@
+// Check if a number is odd or even in JavaScript. 
+
+let a = parseInt(prompt("Enter the first number:"));
+// let b = parseInt(prompt("Enter the secound number:"));
+
+if (a%2==0)
+{
+    window.alert("the number is even");
+}
+else
+{
+    window.alert("the number is odd");
+}
