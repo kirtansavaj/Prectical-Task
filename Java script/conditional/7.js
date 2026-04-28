60f937f35f684b0c81579a85df08c79c022c6f69
kirtansavaj <102339342+kirtansavaj@users.noreply.github.com>
2026-04-28 09:14:07 +0530

    Add files via upload

---
+++ Java script/conditional/7.js
@@ -0,0 +1,17 @@
+// Perform arithmetic operations on two numbers. Operations: + - * / %
+
+let a = 10;
+let b = 12;
+
+let sum = a + b;
+let minus = a -b;
+let mul = a * b;
+let div = a / b;
+let modu = a % b;
+
+
+document.write(sum + "<br>");
+document.write(minus + "<br>");
+document.write(mul + "<br>");
+document.write(div + "<br>");
+document.write(modu + "<br>");
