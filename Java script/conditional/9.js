6634d9937460a204c3b644778906a0435a12b8a5
kirtansavaj <102339342+kirtansavaj@users.noreply.github.com>
2026-04-28 09:14:38 +0530

    Add files via upload

---
+++ Java script/conditional/9.js
@@ -0,0 +1,14 @@
+// Write a program that takes a year from the user and check whether that year is a leap year or not.
+
+
+
+let a = parseInt(prompt("Enter the first number:"));
+
+if (a%4==0)
+{
+    window.alert("the number is leap year");
+}
+else
+{
+    window.alert("the number is not leap year");
+}
