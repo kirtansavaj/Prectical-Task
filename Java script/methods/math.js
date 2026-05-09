afaaacd1d735d08a05a805eb086d75a7a8da04b8
kirtansavaj <102339342+kirtansavaj@users.noreply.github.com>
2026-05-09 09:07:03 +0530

    Create math.js

---
+++ Java script/methods/math.js
@@ -0,0 +1,85 @@
+// Math.sin()
+const sinValue = Math.sin(Math.PI / 2);
+console.log(sinValue);
+
+// Math.cos()
+const cosValue = Math.cos(Math.PI);
+console.log(cosValue);
+
+// Math.tan()
+const tanValue = Math.tan(0);
+console.log(tanValue);
+
+// Math.asin()
+const asinValue = Math.asin(0.5);
+console.log(asinValue);
+
+// Math.acos()
+const acosValue = Math.acos(0.5);
+console.log(acosValue);
+
+// Math.atan()
+const atanValue = Math.atan(1);
+console.log(atanValue);
+
+// Math.atan2()
+const atan2Value = Math.atan2(90, 15);
+console.log(atan2Value);
+
+// Math.exp()
+const expValue = Math.exp(2);
+console.log(expValue);
+
+// Math.log()
+const logValue = Math.log(10);
+console.log(logValue);
+
+// Math.log10()
+const log10Value = Math.log10(100);
+console.log(log10Value);
+
+// Math.log2()
+const log2Value = Math.log2(8);
+console.log(log2Value);
+
+// Math.pow()
+const powValue = Math.pow(2, 3);
+console.log(powValue);
+
+// Math.sqrt()
+const sqrtValue = Math.sqrt(25);
+console.log(sqrtValue);
+
+// Math.cbrt()
+const cbrtValue = Math.cbrt(27);
+console.log(cbrtValue);
+
+// Math.round()
+const roundValue = Math.round(3.6);
+console.log(roundValue);
+
+// Math.floor()
+const floorValue = Math.floor(5.9);
+console.log(floorValue);
+
+// Math.ceil()
+const ceilValue = Math.ceil(4.1);
+console.log(ceilValue);
+
+// Math.abs()
+const absValue = Math.abs(-7);
+console.log(absValue);
+
+// Math.PI
+console.log(Math.PI * 1);
+
+// Math.E
+console.log(Math.E);
+
+// Math.min()
+const minValue = Math.min(5000, 100, 15);
+console.log(minValue);
+
+// Math.max()
+const maxValue = Math.max(20, 30, 40);
+console.log(maxValue);
