971f689cd972164f884193506924bb2af2538383
Kirtan Savaj <102339342+kirtansavaj@users.noreply.github.com>
2026-05-11 09:14:56 +0530

    Add files via upload

---
+++ Java script/operators/typeof.js
@@ -0,0 +1,23 @@
+let y = 4;
+let z = "hello";
+let a = ["a","b","c","d"]; //
+let b = { name: "kirtan", age: 21 };
+let d = 87788788787877878787878787; //
+let e;
+let f = null; // 
+let g = true;
+let h = Symbol('hello');
+function xyz(a, b) {
+    document.write(a*b + "<br>");
+}
+
+document.write(typeof y + "<br>");
+document.write(typeof z + "<br>");
+document.write(typeof a + "<br>");
+document.write(typeof b + "<br>");
+document.write(typeof d + "<br>");
+document.write(typeof e + "<br>");
+document.write(typeof f + "<br>");
+document.write(typeof g + "<br>");
+document.write(typeof h + "<br>");
+document.write(typeof xyz + "<br>");
