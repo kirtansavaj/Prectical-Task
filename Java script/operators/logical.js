a5ea69ddb3ef5a96e48b99bbfd0234fa00de8e44
Kirtan Savaj <102339342+kirtansavaj@users.noreply.github.com>
2026-05-11 09:14:39 +0530

    Add files via upload

---
+++ Java script/operators/logical.js
@@ -0,0 +1,10 @@
+let x = 3;
+let y = 4;
+
+let a = (x < 10 && y > 1);
+let b = (x == 5 || y == 5);
+let c = !(x < y);
+
+document.write(a + "<br>");
+document.write(b + "<br>");
+document.write(c + "<br>");
