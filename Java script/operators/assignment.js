9a911bbd6883ac26494746e5856030e775e8e421
Kirtan Savaj <102339342+kirtansavaj@users.noreply.github.com>
2026-05-11 09:13:50 +0530

    Add files via upload

---
+++ Java script/operators/assignment.js
@@ -0,0 +1,19 @@
+let x = 3;
+let y = 4;
+
+let a = x = y;
+let b = x += y;
+let c  = x -= y;
+let d = x *= y;
+let e = x /= y;
+let f = x %= y;
+let g = x **= y;
+
+
+document.write(a + "<br>");
+document.write(b + "<br>");
+document.write(c + "<br>");
+document.write(d + "<br>");
+document.write(e + "<br>");
+document.write(f + "<br>");
+document.write(g + "<br>");
