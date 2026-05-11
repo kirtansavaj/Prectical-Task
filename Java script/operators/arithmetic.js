16bb308c01540658afe4ccde7823faca6f220374
Kirtan Savaj <102339342+kirtansavaj@users.noreply.github.com>
2026-05-11 09:13:14 +0530

    Add files via upload

---
+++ Java script/operators/arithmetic.js
@@ -0,0 +1,37 @@
+let x = 3;
+let y = 4;
+
+let a = x + y; // sum
+let b = x - y; // sub
+let c = x * y; // mul
+let d = x / y; // divi
+let e = x % y; // mod
+let f = ++x; // incre
+let g = x--; // decre
+let ab = x**y;
+
+
+
+document.write(a + "<br>");
+document.write(b + "<br>");
+document.write(c + "<br>");
+document.write(d + "<br>");
+document.write(e + "<br>");
+document.write(f + "<br>");
+document.write(g + "<br>");
+document.write(ab + "<br>");
+
+
+
+let w = 4;
+let n =4;
+let h = w > n;
+let i = w < n;
+let j = w !== n;
+
+
+
+document.write(h + "<br>");
+document.write(i + "<br>");
+document.write(j + "<br>");
+
