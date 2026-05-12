331d77686d6893e06936e4ddf84d98ad8609c9db
Kirtan Savaj <102339342+kirtansavaj@users.noreply.github.com>
2026-05-12 09:05:29 +0530

    Add files via upload

---
+++ Java script/output/1.js
@@ -0,0 +1,21 @@
+window.alert(5 + 8);
+window.alert(4 - 5);
+window.alert(1 * 6);
+window.alert(8 + 5);
+window.alert(5 - 7);
+window.alert(8 * 6);
+window.alert(6 + 8);
+window.alert(4 - 7);
+window.alert(7 * 6);    
+window.alert(4 + 5);
+window.alert(2 - 3);
+window.alert(9 * 1);
+window.alert(7 + 3);
+window.alert(9 - 7);
+window.alert(9 * 9);
+
+
+// in console - animal name
+// write - birds nmae
+// alert - Math + - * /
+// 15
