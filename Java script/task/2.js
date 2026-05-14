4df1a876a8838d557efc160ebb9b07ed2dbcff41
Kirtan Savaj <102339342+kirtansavaj@users.noreply.github.com>
2026-05-14 09:41:13 +0530

    Add files via upload

---
+++ Java script/task/2.js
@@ -0,0 +1,21 @@
+// Check if a number is a palindrome
+
+let a = prompt("Enter the number");
+let number = parseInt(a);
+let d = number;
+let c = 0;
+
+if (number > 0) {
+    while (number > 0) {
+        let digit = number % 10;
+        c = c * 10 + digit;
+        number = Math.floor(number / 10); 
+
+    }
+    // document.write("reversed number is: " + c);
+    if (c === d) {
+        document.write("Palindrome");
+    } else {
+        document.write("Not a palindrome"); 
+    }
+} 
